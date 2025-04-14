f1e7936d5ee98e215ff3e419feecb1df3198942e
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-04-14 03:31:38 +0100

    feat(users): implement Users module with controller and service, including unit tests

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+  }
+}
