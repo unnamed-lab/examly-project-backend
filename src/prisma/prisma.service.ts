f393bced49d65bee0b5b1452e17599dbd4778128
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-04-14 04:13:56 +0100

    fix: update Prisma import paths and adjust output directory in schema

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
+import { PrismaClient } from 'src/generated/prisma';
 
 @Injectable()
 export class PrismaService
